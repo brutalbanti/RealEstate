76f0e22866fd3c34a9c9e59f1bb56d9ffe57667f
brutalbanti <75171239+brutalbanti@users.noreply.github.com>
2022-08-28 14:48:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.slider-main-block', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.body-main-block__arrow.swiper-button-next',
+    prevEl: '.body-main-block__arrow.swiper-button-prev',
+  }
+});
+
+//Табы
+
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+
+document.addEventListener('click', function (e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+  if (targetElement.closest('.tabs-deals__button')) {
+    tabNavItems.forEach((tabNavItem, index) => {
+      if(tabNavItem.classList.contains('active')) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove('active')
+      }
+      if(tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+      // tabNavItems.classList.remove('active');
+    })
+    targetElement.classList.add('active');
+    tabItems[currentActiveIndex].classList.remove('active');
+    tabItems[newActiveIndex].classList.add('active');
+  }
+});
